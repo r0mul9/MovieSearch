2d4064eda20f84cce287280cc88d486b0c58ca00
romul9 <broo.bro@mail.ru>
2024-08-10 13:43:42 +0300

    Оформили меню кнопок, добавили постеры фильмы и источник

--- app/src/main/java/com/ivantsovdev/moviesearch/MainActivity.kt
+++ app/src/main/java/com/ivantsovdev/moviesearch/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.ivantsovdev.moviesearch
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,28 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
+
+    fun button1(view: View) {
+        Toast.makeText(this, "Главная", Toast.LENGTH_SHORT).show()
+    }
+
+    fun button2(view: View) {
+        Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+    }
+
+    fun button3(view: View) {
+        Toast.makeText(this, "Фильмы", Toast.LENGTH_SHORT).show()
+    }
+
+    fun button4(view: View) {
+        Toast.makeText(this, "Сериалы", Toast.LENGTH_SHORT).show()
+    }
+
+    fun button5(view: View) {
+        Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+    }
+
+
 }
