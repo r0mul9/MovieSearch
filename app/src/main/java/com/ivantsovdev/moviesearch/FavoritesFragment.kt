a5e4f03c4051d49e94e3166d5a647c2d525cf176
romul9 <broo.bro@mail.ru>
2024-10-20 20:09:16 +0300

    added two fragments: “Selections” and “Watch later”, implemented animated transitions between them

--- app/src/main/java/com/ivantsovdev/moviesearch/FavoritesFragment.kt
+++ app/src/main/java/com/ivantsovdev/moviesearch/FavoritesFragment.kt
@@ -29,6 +29,8 @@ class FavoritesFragment : Fragment() {
         //Получаем список при транзакции фрагмента
         val favoritesList: List<Film> = emptyList()
 
+        AnimationHelper.performFragmentCircularRevealAnimation(binding.favoritesFragmentRoot, requireActivity(),2)
+
         binding.favoritesRecycler
             .apply {
                 filmsAdapter =
