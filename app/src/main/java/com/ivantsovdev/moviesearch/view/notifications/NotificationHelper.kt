8cb5c091e2be82a496f36e07cc75bd09bb03a251
romul9 <broo.bro@mail.ru>
2025-03-21 18:03:22 +0300

    Implemented a menu when clicking the "watch later" button

--- app/src/main/java/com/ivantsovdev/moviesearch/view/notifications/NotificationHelper.kt
+++ app/src/main/java/com/ivantsovdev/moviesearch/view/notifications/NotificationHelper.kt
@@ -1,11 +1,15 @@
 package com.ivantsovdev.moviesearch.view.notifications
 
 
+import android.app.AlarmManager
+import android.app.DatePickerDialog
 import android.app.PendingIntent
+import android.app.TimePickerDialog
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
+import android.os.Bundle
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
@@ -14,8 +18,10 @@ import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
 import com.ivantsovdev.moviesearch.R
 import com.ivantsovdev.moviesearch.data.Entity.Film
+import com.ivantsovdev.moviesearch.receivers.ReminderBroadcast
 import com.ivantsovdev.moviesearch.view.MainActivity
 import com.ivantsovdev.remote_module.entity.ApiConstants
+import java.util.Calendar
 
 
 object NotificationHelper {
@@ -55,4 +61,72 @@ object NotificationHelper {
 //Отправляем изначальную нотификацию в стандартном исполнении
         notificationManager.notify(film.id, builder.build())
     }
+
+    fun notificationSet(context: Context, film: Film) {
+        val calendar = Calendar.getInstance()
+        val currentYear = calendar.get(Calendar.YEAR)
+        val currentMonth = calendar.get(Calendar.MONTH)
+        val currentDay = calendar.get(Calendar.DAY_OF_MONTH)
+        val currentHour = calendar.get(Calendar.HOUR_OF_DAY)
+        val currentMinute = calendar.get(Calendar.MINUTE)
+
+        DatePickerDialog(
+            context,
+            { _, dpdYear, dpdMonth, dayOfMonth ->
+                val timeSetListener =
+                    TimePickerDialog.OnTimeSetListener { _, hourOfDay, pickerMinute ->
+                        val pickedDateTime = Calendar.getInstance()
+                        pickedDateTime.set(
+                            dpdYear,
+                            dpdMonth,
+                            dayOfMonth,
+                            hourOfDay,
+                            pickerMinute,
+                            0
+                        )
+                        val dateTimeInMillis = pickedDateTime.timeInMillis
+                        //После того, как получим время, вызываем метод, который создаст Alarm
+                        createWatchLaterEvent(context, dateTimeInMillis, film)
+                    }
+
+                TimePickerDialog(
+                    context,
+                    timeSetListener,
+                    currentHour,
+                    currentMinute,
+                    true
+                ).show()
+
+            },
+            currentYear,
+            currentMonth,
+            currentDay
+        ).show()
+    }
+
+    private fun createWatchLaterEvent(context: Context, dateTimeInMillis: Long, film: Film) {
+        //Получаем доступ к AlarmManager
+        val alarmManager =
+            context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        //Создаем интент для запуска ресивера
+        val intent = Intent(film.title, null, context, ReminderBroadcast()::class.java)
+        //Кладем в него фильм
+        val bundle = Bundle()
+        bundle.putParcelable(NotificationConstants.FILM_KEY, film)
+        intent.putExtra(NotificationConstants.FILM_BUNDLE_KEY, bundle)
+        //Создаем пендинг интент для запуска извне приложения
+        val pendingIntent = PendingIntent.getBroadcast(
+            context,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        //Устанавливаем Alarm
+        alarmManager.setExact(
+            AlarmManager.RTC_WAKEUP,
+            dateTimeInMillis,
+            pendingIntent
+        )
+    }
+
 }
